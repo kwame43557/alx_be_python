5c3c9998f0980cc8c1ed48766f72d5a31f0addef
kwame43557 <feelwillow@gmail.com>
2025-08-26 17:03:22 +0100

    Added Simple Bank Bank

---
+++ programming_paradigm/bank_account.py
@@ -0,0 +1,17 @@
+
+class BankAccount:
+    def __init__(self, initial_balance=0):
+        """Initialize the bank account with an optional starting balance."""
+        self.__account_balance = initial_balance
+
+    def deposit(self, amount):
+        self.__account_balance += amount
+
+    def withdraw(self, amount):
+        if amount <= self.__account_balance:
+            self.__account_balance -= amount
+            return True
+        return False
+
+    def display_balance(self):
+        print(f"Current Balance: ${self.__account_balance}")
